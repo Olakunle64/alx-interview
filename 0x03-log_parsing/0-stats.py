0c0eac12e2088f732522906326bab86fee23bcd5
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-01 02:43:01 +0100

    correct pycodestyle error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module has a script that reads stdin line by line and computes metrics"""
+"""This module has a script that reads stdin line by
+    line and computes metrics
+"""
 import signal
 import re
 import sys
@@ -18,9 +20,11 @@ def customPrintHandler(signal, frame, status_code, file_size):
     for key, value in status_code.items():
         print("{}: {}".format(key, value))
 
+
 def customPrintHandlerWrapper(signal, frame, status_code, file_size):
     """A custom print handler wrapper"""
     customPrintHandler(signal, frame, status_code, file_size)
+
 
 status_code = {"200": 0, "301": 0, "400": 0,
                "401": 0, "403": 0, "404": 0,
@@ -28,7 +32,8 @@ status_code = {"200": 0, "301": 0, "400": 0,
                }
 file_size = 0
 keep_track = 0
-pattern = re.compile(r'''([0-2]?[0-9]?[0-9]\.){3}([0-2]?[0-9]?[0-9])\s- # matches the ip address
+pattern = re.compile(r'''([0-2]?[0-9]?[0-9]\.){3}([0-2]?[0-9]?[0-9])\s-
+                     # matches the ip address
                      \s\[[\d]{1,4}- # matches the year in datetime
                      ([0-1]\d)- # matches the month in datetime
                      ([0-3]?\d)\s # matches the day in datetime

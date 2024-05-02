29bf71bbf8faaf0be858eaa339b8efb06b2db6b7
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-02 15:25:04 +0100

    parse user input and display the statistic

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,59 +5,65 @@
 import signal
 import re
 import sys
+from datetime import datetime
 
 
-def customPrint(status_code=None, file_size=0):
+status_codes = {
+    "200": 0, "301": 0, "400": 0,
+    "401": 0, "403": 0, "404": 0,
+    "405": 0, "500": 0
+    }
+
+total_file_size = 0
+
+
+def customPrint(signal=0, frame=0):
     """A custom print"""
-    print("File size: {}".format(file_size))
-    for key, value in status_code.items():
+    print("File size: {}".format(total_file_size))
+    for key, value in status_codes.items():
         print("{}: {}".format(key, value))
 
 
-def customPrintHandler(signal, frame, status_code, file_size):
-    """custom print handler for KeyboardInterrupt"""
-    print("File size: {}".format(file_size))
-    for key, value in status_code.items():
-        print("{}: {}".format(key, value))
-
-
-def customPrintHandlerWrapper(signal, frame, status_code, file_size):
-    """A custom print handler wrapper"""
-    customPrintHandler(signal, frame, status_code, file_size)
-
-
-status_code = {"200": 0, "301": 0, "400": 0,
-               "401": 0, "403": 0, "404": 0,
-               "405": 0, "500": 0
-               }
-file_size = 0
-keep_track = 0
-pattern = re.compile(r'''([0-2]?[0-9]?[0-9]\.){3}([0-2]?[0-9]?[0-9])\s-
+# regex pattern to match user input
+pattern = re.compile(r'''(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s-
                      # matches the ip address
-                     \s\[[\d]{1,4}- # matches the year in datetime
-                     ([0-1]\d)- # matches the month in datetime
-                     ([0-3]?\d)\s # matches the day in datetime
-                     ([0-2][\d]): # matches the hour in datetime
-                     ([0-6][\d]): # matches the minutes in datetime
-                     ([0-6][\d]). # matches the seconds in datetime
-                     \d+\]\s # matches the miliseconds in datetime
+                     \s\[([\d]{1,4}- # matches the year in datetime
+                     \d{2}- # matches the month in datetime
+                     \d{2}\s # matches the day in datetime
+                     \d{2}: # matches the hour in datetime
+                     \d{2}: # matches the minutes in datetime
+                     \d{2}. # matches the seconds in datetime
+                     \d+)\]\s # matches the miliseconds in datetime
                      "GET\s/projects/260\sHTTP/1.1"\s # matches the route
-                     ([2-5]0[0-5])\s # matches the status code
+                     (\d{3})\s # matches the status code
                      ([\d]+) # matches the file size
                      ''', re.VERBOSE)
+
+keep_track = 0
 for line in sys.stdin:
-    if keep_track == 10:
-        customPrint(status_code=status_code, file_size=file_size)
-        keep_track = 0
     match = pattern.search(line.strip())
     if match:
         matched_groups = match.groups()
-        file_size += int(matched_groups[-1].strip())
-        if matched_groups[-2].strip() in status_code.keys():
-            status_code[matched_groups[-2].strip()] += 1
-    signal.signal(
-        signal.SIGINT, lambda signal, frame: customPrintHandlerWrapper(
-            signal, frame, status_code, file_size
-            )
-            )
-    keep_track += 1
+        ips = matched_groups[0].split(".")
+        status = matched_groups[2]
+        file_size = int(matched_groups[3])
+        if (
+            any(
+                int(ip) < 1 or int(ip) > 255 for ip in ips
+                ) or status not in status_codes.keys() or
+                (file_size < 1 or file_size > 1024)):
+            continue
+        date = matched_groups[1]
+        try:
+            # check if datetime is valid
+            datetime.fromisoformat(date)
+        except Exception:
+            continue
+        total_file_size += file_size
+        status_codes[status] += 1
+        keep_track += 1
+        if keep_track == 10:
+            customPrint()
+            keep_track = 0
+        # catch the keyboardinterrupt signal
+        signal.signal(signal.SIGINT, customPrint)

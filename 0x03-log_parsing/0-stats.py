e47f6ea8bf20b870480e6cd11eb27be013e9a0ee
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-02 22:34:26 +0100

    debugging..

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -55,8 +55,7 @@ if __name__ == "__main__":
                 if (
                     any(
                         int(ip) < 1 or int(ip) > 255 for ip in ips
-                        ) or status not in status_codes.keys() or
-                        (file_size < 1 or file_size > 1024)):
+                        ) or status not in status_codes.keys()):
                     continue
                 date = matched_groups[1]
                 # try:

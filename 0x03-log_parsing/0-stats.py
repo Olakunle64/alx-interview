783d77526a687e27e7b66ce8aaea299e9de20c43
Olakunle64 <133286231+Olakunle64@users.noreply.github.com>
2024-05-03 11:58:47 +0100

    Update 0-stats.py
    do not compulsory a space around the hyphen separating the ip and the date

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -24,9 +24,9 @@ def customPrint() -> None:
 
 
 # regex pattern to match user input
-pattern = re.compile(r'''(^[\w:.]+)\s-
+pattern = re.compile(r'''(^[\w:.]+)\s?-
                      # matches the ip address
-                     \s\[([\d]{1,4}- # matches the year in datetime
+                     \s?\[([\d]{1,4}- # matches the year in datetime
                      \d{2}- # matches the month in datetime
                      \d{2}\s # matches the day in datetime
                      \d{2}: # matches the hour in datetime

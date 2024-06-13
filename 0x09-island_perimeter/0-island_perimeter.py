ed24facf6f3489511fffecb33e2e81b24f4f5103
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-13 01:31:18 +0100

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module has a function named island_perimeter"""
+
+
+def island_perimeter(grid):
+    """This function returns the perimeter
+        of the island described in grid
+    """
+    if not grid or not grid[0]:
+        return 0
+    perimeter = 0
+    row = 1
+    while row < len(grid):
+        col = 1
+        while col < len(grid[row]):
+            if grid[row][col] == 1:
+                tempPerimeter = 4
+
+                # check right
+                if col + 1 < len(grid[row]):
+                    if grid[row][col + 1] == 1:
+                        tempPerimeter -= 1
+
+                # check left
+                if grid[row][col - 1] == 1:
+                    tempPerimeter -= 1
+
+                # check up
+                if grid[row - 1][col] == 1:
+                    tempPerimeter -= 1
+
+                # check down
+                if row + 1 < len(grid):
+                    if grid[row + 1][col] == 1:
+                        tempPerimeter -= 1
+                perimeter += tempPerimeter
+                
+            col += 1
+        row += 1
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,1 @@
+island perimeter technical interview
